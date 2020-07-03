2be9e19fe6a626d0f5b927efe94d227d7b2921c9
Adrian Sadowski <adrian.sadowski@schibsted.com>
2020-07-03 10:28:34 +0200

    Added async to netlify identity widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <article>
         <h1>{title}</h1>
