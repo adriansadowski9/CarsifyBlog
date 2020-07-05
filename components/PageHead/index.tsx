1942d589053cef7ab355a3ae4ac0b8b9a81281f7
Adrian Sadowski <adrian.sadowski@schibsted.com>
2020-07-05 11:45:57 +0200

    Changed favicon path

--- components/PageHead/index.tsx
+++ components/PageHead/index.tsx
@@ -10,7 +10,7 @@ const PageHead = ({ title, description }: PageHeadProps) => (
     <meta charSet="UTF-8" />
     <meta name="description" content={description} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="../../public/favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" />
   </Head>
 )
 
