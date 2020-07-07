121efcf4074ccb493c308518bf810ef01b488e3c
Adrian Sadowski <adrian.sadowski@schibsted.com>
2020-07-07 14:10:54 +0200

    Added Maven Pro font and light/dark themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
 import { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import useDarkMode from 'use-dark-mode'
 import Layout from 'components/Layout'
-import { lightTheme, darkTheme } from 'public/assets/theme';
+import { lightTheme, darkTheme } from 'public/assets/theme'
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Montserrat';
+      src: url('/assets/fonts/MavenPro-Regular.ttf');
+      src: url('/static/fonts/MavenPro-Medium.ttf');
+      src: url('/static/fonts/MavenPro-Bold.ttf');
+  }
+  * {
+    font-family: 'Maven Pro', sans-serif;
+  }
+`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const darkMode = useDarkMode(true)
   const theme = darkMode.value ? darkTheme : lightTheme
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
       <Layout darkModeEnabled={darkMode.value} enableDarkMode={darkMode.enable} disableDarkMode={darkMode.disable}>
         <Component {...pageProps} />
       </Layout>
--- public/assets/theme.ts
+++ public/assets/theme.ts
@@ -1,9 +1,108 @@
+const colors = {
+  white: '#FFFFFF',
+  black: '#0D0D0D',
+  red: '#C20708',
+  silver: '#CCD4DA',
+  darkSilver: '#A4B3BF',
+  gray: '#A6A6A6',
+  darkGray: '#262626'
+}
+
+const fontSizes = {
+  xs: 12,
+  s: 14,
+  m: 16,
+  l: 20,
+  xl: 24,
+  xxl: 48
+}
+
+const fontWeights = {
+  regular: 400,
+  medium: 500,
+  bold: 700
+}
+
+const spaces = {
+  xxs: 10,
+  xs: 15,
+  s: 20,
+  m: 25,
+  l: 30,
+  xl: 40,
+  xxl: 60,
+}
+
 export const lightTheme = {
-  bg: 'white',
-  text: 'black'
+  fontSizes,
+  fontWeights,
+  spaces,
+  colors: {
+    bg: colors.white,
+    text: colors.black,
+    sectionText: colors.gray,
+    border: colors.silver,
+    categoryName: colors.gray,
+    iconCircleBg: colors.silver,
+    icon: colors.black,
+    tipCategoryName: colors.black,
+    tipIconCircleBg: colors.black,
+    tipIcon: colors.white,
+    menuActiveItem: colors.red,
+    hamburger: colors.red,
+    menuArrow: colors.red,
+    subtitleText: colors.gray,
+    bulletActive: colors.white,
+    bullet: colors.silver,
+    tipCardBg: colors.silver,
+    adCarBoxBg: colors.darkGray,
+    adLocalization: colors.red,
+    adPrice: colors.red,
+    shareSection: colors.red,
+    moreSection: colors.red,
+    categoryBoxActiveBg: colors.red,
+    categoryBoxActiveText: colors.white,
+    categoryBoxBg: colors.darkSilver,
+    categoryBoxActive: colors.black,
+    inputBorder: colors.darkGray,
+    submitButton: colors.red,
+    socialButton: colors.darkGray
+  }
 }
 
 export const darkTheme = {
-  bg: 'black',
-  text: 'white'
+  fontSizes,
+  fontWeights,
+  spaces,
+  colors: {
+    bg: colors.darkGray,
+    text: colors.white,
+    sectionText: colors.gray,
+    border: colors.silver,
+    categoryName: colors.gray,
+    iconCircleBg: colors.silver,
+    icon: colors.black,
+    tipCategoryName: colors.black,
+    tipIconCircleBg: colors.black,
+    tipIcon: colors.white,
+    menuActiveItem: colors.red,
+    hamburger: colors.red,
+    menuArrow: colors.red,
+    subtitleText: colors.gray,
+    bulletActive: colors.white,
+    bullet: colors.silver,
+    tipCardBg: colors.silver,
+    adCarBoxBg: colors.black,
+    adLocalization: colors.red,
+    adPrice: colors.red,
+    shareSection: colors.red,
+    moreSection: colors.red,
+    categoryBoxActiveBg: colors.red,
+    categoryBoxActiveText: colors.white,
+    categoryBoxBg: colors.darkSilver,
+    categoryBoxActive: colors.black,
+    inputBorder: colors.white,
+    submitButton: colors.red,
+    socialButton: colors.black
+  }
 }
