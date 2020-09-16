3d46948e6f218422a48754735ea2a5fe039a2847
mateki0 <mateki0@interia.pl>
2020-09-16 18:11:14 +0200

    chevron wrapper changed to button, added needed styles

--- components/Header/styled/ChevronWrapper.tsx
+++ components/Header/styled/ChevronWrapper.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
-const ChevronWrapper = styled.div<{ isOpen: boolean }>`
+const ChevronWrapper = styled.button<{ isOpen: boolean }>`
   position: absolute;
   left: ${(props) => props.theme.spaces.l};
   transition: transform 0.5s;
   transform: ${(props) => (props.isOpen ? 'rotate(180deg)' : 'none')};
+  border: none;
+  background: transparent;
+  outline: 0;
   @media only screen and (min-width: ${(props) => props.theme.breakpoints[0]}) {
     display: none;
     visibility: hidden;
