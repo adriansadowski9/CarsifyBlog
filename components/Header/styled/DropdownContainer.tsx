5d07f9c5ac6993bc8ecf45e5ac6e1ab28a8fef5b
mateki0 <mateki0@interia.pl>
2020-10-18 13:24:01 +0200

    dropdown closing on desktop fixed

--- components/Header/styled/DropdownContainer.tsx
+++ components/Header/styled/DropdownContainer.tsx
@@ -15,7 +15,8 @@ const DropdownContainer = styled.ul<{ isOpen: boolean }>`
   background-color: ${(props) => props.theme.colors.bg};
   transition: opacity 0.5s;
   @media only screen and (min-width: ${(props) => props.theme.breakpoints[1]}) {
-    opacity: 1;
+    opacity: 0;
+    visible: hidden;
     padding: ${(props) => props.theme.spaces.xxs} 0;
     height: auto;
     box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.25);
