b1c9961e0462c4fb8b7c9709fd46f5f267126a50
mateki0 <mateki0@interia.pl>
2020-10-22 15:56:06 +0200

    grid gap, max width and padding changed

--- components/Layout/styled/Row.tsx
+++ components/Layout/styled/Row.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Row = styled.div`
   display: grid;
 
-  grid-gap: 30px;
+  grid-gap: 40px;
   @media only screen and (min-width: ${(props) => props.theme.breakpoints[0]}) {
     grid-template-columns: 820px;
   }
