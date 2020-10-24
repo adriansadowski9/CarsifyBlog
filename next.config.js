39e96a39a2186d8cae18ae3acb6333deca858b17
luvisoniasty <luvisonx@gmail.com>
2020-10-24 13:29:02 +0200

    Removed unnecessary responsive loader

--- next.config.js
+++ next.config.js
@@ -1,40 +1,37 @@
-const withFonts = require('next-fonts');
-const withOptimizedImages = require('next-optimized-images');
-const withPWA = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
+const fonts = require('next-fonts');
+const optimizedImages = require('next-optimized-images');
+const pwa = require('next-pwa');
 
-module.exports = withPWA(
-  withOptimizedImages(
-    withFonts({
-      imagesName: '[name]-doubled-image-[hash].[ext]',
-      target: 'serverless',
-      pwa: {
+module.exports = withPlugins(
+  [
+    [
+      pwa,
+      {
+        disable: process.env.NODE_ENV === 'development',
         dest: 'public',
-        publicExcludes: ['!-doubled-image-*.*'],
         sw: 'service-worker.js',
       },
-      webpack: (cfg) => {
-        cfg.module.rules.push({
-          test: /\.md$/,
-          loader: 'frontmatter-markdown-loader',
-        });
-        cfg.module.rules.push({
-          test: /\.(jpe?g|png)$/i,
-          loader: 'responsive-loader',
-          options: {
-            adapter: require('responsive-loader/sharp'),
-          },
-        });
-        cfg.module.rules.map((rule) => {
-          if (rule.test !== undefined && rule.test.source.includes('|svg|')) {
-            rule.test = new RegExp(rule.test.source.replace('|svg|', '|'));
-          }
-        });
-        cfg.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        });
-        return cfg;
-      },
-    })
-  )
+    ],
+    [optimizedImages],
+    [fonts],
+  ],
+  {
+    webpack: (cfg) => {
+      cfg.module.rules.push({
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+      });
+      cfg.module.rules.map((rule) => {
+        if (rule.test !== undefined && rule.test.source.includes('|svg|')) {
+          rule.test = new RegExp(rule.test.source.replace('|svg|', '|'));
+        }
+      });
+      cfg.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+      return cfg;
+    },
+  }
 );
