bcb538445d11e51c963d5fe281ee0ca97eb5787b
luvisoniasty <luvisonx@gmail.com>
2020-10-24 14:15:05 +0200

    Removed next compose plugins and turned off pva in dev

--- next.config.js
+++ next.config.js
@@ -1,42 +1,34 @@
-const withPlugins = require('next-compose-plugins');
-const fonts = require('next-fonts');
-const optimizedImages = require('next-optimized-images');
-const pwa = require('next-pwa');
+const withFonts = require('next-fonts');
+const withOptimizedImages = require('next-optimized-images');
+const withPWA = require('next-pwa');
 
-module.exports = withPlugins(
-  [
-    [
-      pwa,
-      {
+module.exports = withPWA(
+  withOptimizedImages(
+    withFonts({
+      target: 'serverless',
+      pwa: {
+        disable: process.env.NODE_ENV === 'development',
         dest: 'public',
       },
-    ],
-    [
-      optimizedImages,
-      {
-        responsive: {
-          adapter: require('responsive-loader/sharp'),
-        },
+      responsive: {
+        adapter: require('responsive-loader/sharp'),
       },
-    ],
-    [fonts],
-  ],
-  {
-    webpack: (cfg) => {
-      cfg.module.rules.push({
-        test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
-      });
-      cfg.module.rules.map((rule) => {
-        if (rule.test !== undefined && rule.test.source.includes('|svg|')) {
-          rule.test = new RegExp(rule.test.source.replace('|svg|', '|'));
-        }
-      });
-      cfg.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      return cfg;
-    },
-  }
+      webpack: (cfg) => {
+        cfg.module.rules.push({
+          test: /\.md$/,
+          loader: 'frontmatter-markdown-loader',
+        });
+        cfg.module.rules.map((rule) => {
+          if (rule.test !== undefined && rule.test.source.includes('|svg|')) {
+            rule.test = new RegExp(rule.test.source.replace('|svg|', '|'));
+          }
+        });
+        cfg.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        });
+        return cfg;
+      },
+    })
+  )
 );
