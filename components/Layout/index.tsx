8392a6735e6c1ec9ec11de599e99842acae634a9
luvisoniasty <luvisonx@gmail.com>
2020-11-05 16:51:06 +0100

    Added alert to layout (possibility to use on every supage) and moved error alert to contact page

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { useRouter } from 'next/router';
 
+import Alert from '@components/Alert';
 import Header from '@components/Header';
 import MainContent from '@components/Layout/styled/MainContent';
 import { ArticleCategory } from '@pages/artykuly/[id]';
@@ -11,11 +13,27 @@ interface LayoutProps {
   tipCategories: TipCategory[];
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, articleCategories, tipCategories }) => (
-  <>
-    <Header articleCategories={articleCategories} tipCategories={tipCategories} />
-    <MainContent>{children}</MainContent>
-  </>
-);
+const Layout: React.FC<LayoutProps> = ({ children, articleCategories, tipCategories }) => {
+  const {
+    query: { alertType, alertHeading, alertMessage },
+  } = useRouter();
+  return (
+    <>
+      <Header articleCategories={articleCategories} tipCategories={tipCategories} />
+      <MainContent>
+        {alertType && alertMessage && (
+          <Alert
+            type={alertType === 'error' ? 'error' : 'success'}
+            message={{
+              heading: alertHeading ? alertHeading.toString() : undefined,
+              text: alertMessage.toString(),
+            }}
+          />
+        )}
+        {children}
+      </MainContent>
+    </>
+  );
+};
 
 export default Layout;
