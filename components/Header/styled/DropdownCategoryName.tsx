21f9984f39c33170a164b8fc17edbc0774b472f0
luvisoniasty <luvisonx@gmail.com>
2021-01-13 21:29:05 +0100

    Added theme values in DropdownCategoryName

--- components/Header/styled/DropdownCategoryName.tsx
+++ components/Header/styled/DropdownCategoryName.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const DropdownCategoryName = styled.span`
   font-size: ${(props) => props.theme.fontSizes.xxl};
   font-weight: ${(props) => props.theme.fontWeights.medium};
-  margin-top: 20px;
-  margin-left: 40px;
-  margin-bottom: 10px;
+  margin-top: ${(props) => props.theme.spaces.s};
+  margin-left: ${(props) => props.theme.spaces.xl};
+  margin-bottom: ${(props) => props.theme.spaces.xxs};
 `;
 export default DropdownCategoryName;
