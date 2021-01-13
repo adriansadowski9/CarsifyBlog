8226f6468f8c4e154b75c91ef778a6b7043eba8b
luvisoniasty <luvisonx@gmail.com>
2021-01-13 21:42:26 +0100

    Used theme value in SearchResultsContainer

--- components/Sections/SearchResultsContainer.tsx
+++ components/Sections/SearchResultsContainer.tsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 const SearchResultsContainer = styled.div`
   display: grid;
-  column-gap: 20px;
+  column-gap: ${(props) => props.theme.spaces.s};
+
   @media only screen and (min-width: ${(props) => props.theme.breakpoints[0]}) {
     grid-template-columns: repeat(2, 1fr);
   }
+
   @media only screen and (min-width: ${(props) => props.theme.breakpoints[1]}) {
     grid-template-columns: repeat(3, 1fr);
   }
